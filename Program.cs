e957c71b1264b361bf0b44b8be770aa0808fb8d2
Nathan Davis <davis.nated@gmail.com>
2025-03-26 21:10:48 -0600

    Remove connector

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
-builder.Services.AddScoped<Connector>();
 
 var app = builder.Build();
 
